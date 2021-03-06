5ca7b84e33483553c2f94389bdc617bd536d8213
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2021-03-06 18:50:07 +0700

    Update unzip.cmd

--- unzip.cmd
+++ unzip.cmd
@@ -5,6 +5,5 @@ title unzip
 echo please enter password
 echo not have password yet? contact here
 set /p pass=password: 
-7z.exe x Contact_0932152322_for_password.zip
-::7z.exe x Contact_0932152322_for_password.zip -p%pass%
+7z.exe x Contact_0932152322_for_password.zip -p%pass%
 exit
