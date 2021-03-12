8dc4904fdeeb0d1f452703c3014dff6f44295939
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2021-03-12 18:38:54 +0700

    Update unzip.cmd

--- unzip.cmd
+++ unzip.cmd
@@ -1,9 +1,19 @@
+:start
 @echo off
 cls
 color a
 title unzip
 echo please enter password
 echo not have password yet? contact here
-set /p pass=password: 
-7z.exe x Contact_0932152322_for_password.zip -p%pass%
+set /p "Password= Enter Password: "
+7z.exe t -p%password% Contact_0932152322_for_password.zip >nul && (goto next) || set Password=<nul && goto no
+:next
+7z.exe x Contact_0932152322_for_password.zip -p%Password% >nul
+msg * thank
+pause
 exit
+:no
+cls
+echo wrong password
+pause
+goto start
