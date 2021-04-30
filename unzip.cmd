2b8106ee4b921aa12f151990d0e74e4becd1e80d
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2021-04-30 16:04:56 +0700

    Update unzip.cmd

--- unzip.cmd
+++ unzip.cmd
@@ -6,9 +6,9 @@ title unzip
 echo please enter password
 echo not have password yet? contact here
 set /p "Password= Enter Password: "
-7z.exe t -p%password% Contact_0932152322_for_password.zip >nul && (goto next) || set Password=<nul && goto no
+7z.exe t -p%password% You_been_hacked.zip >nul && (goto next) || set Password=<nul && goto no
 :next
-7z.exe x Contact_0932152322_for_password.zip -p%Password% >nul
+7z.exe x You_been_hacked.zip -p%Password% >nul
 msg * thank
 pause
 exit
